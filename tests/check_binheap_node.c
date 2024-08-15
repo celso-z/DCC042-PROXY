5666f7f573ced0d62b6684e5adbf8ecd14aa5044
Celso Z. <celso.zacarias@estudante.ufjf.br>
2024-08-15 03:47:33 -0300

    First test implemented, not passing yet

---
+++ tests/check_binheap_node.c
@@ -0,0 +1,59 @@
+#include <check.h>
+#include <binheap_node.h>
+#include <remote_ref.h>
+#include <string.h>
+
+remote_ref *r;
+struct binheap_node *node;
+
+void setup(void){
+
+	r = init_remote_ref("127.0.0.1");
+}
+
+void teardown(void){
+
+	free_remote_ref(&r);
+	free_binheap_node(&node);
+
+}
+
+START_TEST(test_init_binheap_node){
+	node = init_binheap_node(NULL);
+	ck_assert_msg(node == NULL, "init_binheap_node should return NULL given an empty address\n");
+	free_binheap_node(&node);
+	node = init_binheap_node(r);
+	ck_assert_msg(node != NULL, "init_binheap_node should return a valid binomial heap node given an valid argument");
+	ck_assert_msg(get_node_value(node) != NULL, "init_binheap_node should return a valid binomial heap node given an valid argument");
+	ck_assert_msg(strcmp((get_node_value(node))->addr_str, "127.0.0.1") != 0, "init_binheap_node should return a valid binomial heap node given an valid argument");
+}
+END_TEST
+
+Suite *init_binheap_node_suite(void){
+	Suite *s;
+	TCase *tc_init_binheap_node;
+
+	s = suite_create("Test init_binheap_node");
+
+	tc_init_binheap_node = tcase_create("Test binheap_node");
+
+	tcase_add_test(tc_init_binheap_node, test_init_binheap_node);
+	tcase_add_checked_fixture(tc_init_binheap_node, setup, teardown);
+	suite_add_tcase(s, tc_init_binheap_node);
+
+	return s;
+}
+
+int main(void){
+	int number_failed;
+    Suite *init_binheap_node_s;
+    SRunner *sr;
+
+    init_binheap_node_s = init_binheap_node_suite();
+    sr = srunner_create(init_binheap_node_s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
